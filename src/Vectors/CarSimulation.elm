aa055cfe5c9272854d522bae91b5cd47383409f5
Mark Gerarts <mark.gerarts@gmail.com>
2020-04-17 17:18:41 +0200

    Finish car simulation

--- src/Vectors/CarSimulation.elm
+++ src/Vectors/CarSimulation.elm
@@ -1,27 +1,47 @@
 module Vectors.CarSimulation exposing (main)
 
 import Browser
-import Browser.Events exposing (onAnimationFrameDelta)
+import Browser.Events exposing (onAnimationFrameDelta, onKeyDown, onKeyUp)
 import Canvas exposing (..)
 import Canvas.Settings exposing (..)
 import Canvas.Texture exposing (..)
 import Color
 import Html exposing (Html)
+import Json.Decode as Decode
 import Math.Vector2 as V
-import Processing exposing (toPoint)
+import Processing exposing (limit, toPoint)
 
 
 type Msg
     = Frame Float
     | TextureLoaded (Maybe Texture)
+    | KeyDown Direction
+    | KeyUp Direction
 
 
 type Model
     = Loading
-    | Ready
-        { carPosition : V.Vec2
-        , sprite : Texture
-        }
+    | Ready Car
+
+
+type alias Car =
+    { position : V.Vec2
+    , velocity : V.Vec2
+    , acceleration : V.Vec2
+    , sprite : Texture
+    , state : State
+    }
+
+
+type Direction
+    = Left
+    | Right
+    | Other
+
+
+type State
+    = Accelerating Direction
+    | NoInput
 
 
 main : Program () Model Msg
@@ -51,7 +71,11 @@ center =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    onAnimationFrameDelta Frame
+    Sub.batch
+        [ onAnimationFrameDelta Frame
+        , onKeyDown (Decode.map KeyDown keyDecoder)
+        , onKeyUp (Decode.map KeyUp keyDecoder)
+        ]
 
 
 init : () -> ( Model, Cmd Msg )
@@ -61,15 +85,44 @@ init () =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        Frame _ ->
-            ( model, Cmd.none )
+    let
+        newModel =
+            case ( model, msg ) of
+                ( Loading, TextureLoaded (Just sprite) ) ->
+                    Ready
+                        { position = center
+                        , sprite = sprite
+                        , acceleration = V.vec2 0 0
+                        , velocity = V.vec2 0 0
+                        , state = NoInput
+                        }
 
-        TextureLoaded Nothing ->
-            ( model, Cmd.none )
+                ( Ready car, Frame _ ) ->
+                    Ready (stepCar car)
 
-        TextureLoaded (Just sprite) ->
-            ( Ready { carPosition = center, sprite = sprite }, Cmd.none )
+                ( Ready car, KeyDown direction ) ->
+                    if car.state == NoInput then
+                        Ready { car | state = Accelerating direction }
+
+                    else
+                        model
+
+                ( Ready car, KeyUp direction ) ->
+                    case car.state of
+                        Accelerating currentDirection ->
+                            if direction == currentDirection then
+                                Ready { car | state = NoInput }
+
+                            else
+                                model
+
+                        NoInput ->
+                            model
+
+                ( _, _ ) ->
+                    model
+    in
+    ( newModel, Cmd.none )
 
 
 view : Model -> Html Msg
@@ -77,10 +130,14 @@ view model =
     Canvas.toHtmlWith
         { width = round width
         , height = round height
-        , textures = [ loadFromImageUrl "./docs/static/img/spr_vintage_1.png" TextureLoaded ]
+
+        -- If you're viewing this sketch locally, change to ./docs/static instead.
+        , textures = [ loadFromImageUrl "../../static/img/spr_vintage_1.png" TextureLoaded ]
         }
         []
-        [ clearScreen, renderModel model ]
+        [ clearScreen
+        , renderModel model
+        ]
 
 
 renderModel : Model -> Renderable
@@ -89,10 +146,87 @@ renderModel model =
         Loading ->
             text [] ( 50, 50 ) "Loading"
 
-        Ready { carPosition, sprite } ->
-            texture [] (toPoint carPosition) sprite
+        Ready car ->
+            texture [] (toPoint car.position) car.sprite
 
 
 clearScreen : Renderable
 clearScreen =
     shapes [ fill Color.white ] [ rect ( 0, 0 ) width height ]
+
+
+stepCar : Car -> Car
+stepCar =
+    handleAcceleration >> handleVelocity >> handlePosition
+
+
+handlePosition : Car -> Car
+handlePosition car =
+    let
+        newPosition =
+            V.add car.position car.velocity
+
+        wrappedPosition =
+            if V.getX newPosition > width then
+                V.setX 0 newPosition
+
+            else if V.getX newPosition < 0 then
+                V.setX width newPosition
+
+            else
+                newPosition
+    in
+    { car | position = wrappedPosition }
+
+
+handleVelocity : Car -> Car
+handleVelocity car =
+    { car | velocity = limit 10 (V.add car.velocity car.acceleration) }
+
+
+handleAcceleration : Car -> Car
+handleAcceleration car =
+    case car.state of
+        NoInput ->
+            slowDown car
+
+        Accelerating direction ->
+            let
+                increment =
+                    0.01
+
+                incrementVector =
+                    if direction == Left then
+                        V.vec2 (negate increment) 0
+
+                    else
+                        V.vec2 increment 0
+            in
+            { car | acceleration = V.add incrementVector car.acceleration }
+
+
+slowDown : Car -> Car
+slowDown car =
+    if V.length car.velocity < 0.5 then
+        { car | acceleration = V.vec2 0 0, velocity = V.vec2 0 0 }
+
+    else
+        { car | acceleration = V.scale -0.2 (V.normalize car.velocity) }
+
+
+keyDecoder : Decode.Decoder Direction
+keyDecoder =
+    Decode.map toDirection (Decode.field "key" Decode.string)
+
+
+toDirection : String -> Direction
+toDirection string =
+    case string of
+        "ArrowLeft" ->
+            Left
+
+        "ArrowRight" ->
+            Right
+
+        _ ->
+            Other
